22bbe47817d8c8d1432a2be446e943aca610fc57
Sushant Saykad <69181604+SushantSaykad@users.noreply.github.com>
2026-04-27 11:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // getters
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  // method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+}
+
+class Square extends Rectangle {
+	 constructor(side) {
+    super(side, side); // call parent constructor
+  }
+
+  // method to calculate perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
